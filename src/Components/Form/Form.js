2b5c530de01ce30155c8e784f51db02b3818f74c
Natalie Vargas <nvargas@ms.icstars.org>
2021-05-16 15:31:46 -0500

    added alerts to conatact page

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,52 +3,53 @@ import emailjs from 'emailjs-com';
 import './Form.css';
 
 export default function Form(){
+    
+
 
     function sendEmail(e){
         e.preventDefault();
 
-        emailjs.sendForm('nvgmail', 'template_c3ssf09', e.target, 'user_P9aMdRJjpC1pDpnDb3uOJ')
-        .then((result) => {
-          console.log(result.text);
-        }, (error) => {
-          console.log(error.text);
-        });
-        e.target.reset()
+        let w= document.getElementById('input1');
+        let x= document.getElementById('input2');
+        let y= document.getElementById('input3');
+        let z=document.getElementById('input4');
+
+
+        if ((w.value=="") || (x.value == "") || (y.value == "") || (z.value == "")){
+            alert("Plase fill all required fields.");
+            return false;
+
+          
+
+
+
+        }else{
+            alert("Thank you for your submission. We'll get back to you soon.")
+
+
+            //send email using emailJS
+            emailjs.sendForm('nvgmail', 'template_c3ssf09', e.target, 'user_P9aMdRJjpC1pDpnDb3uOJ')
+            .then((result) => {
+              console.log(result.text);
+            }, (error) => {
+              console.log(error.text);
+            });
+            e.target.reset()
+
+            return true;
+
+        }
+
+       
         
     }
-
-    //send alert message if any field is empty and send alert with thank you if all fields are filled
-function myFunction(){
-
-    //gets html object
-    let w= document.getElementById('input1');
-    let x= document.getElementById('input2');
-    let y= document.getElementById('input3');
-    let z=document.getElementById('input4');
-    
-    //gets value of object and checks is it's empty
-    //quotes represent empty field
-    if ((w.value=="") || (x.value == "") || (y.value == "") || (z.value == "")){
-        Form();
-        alert("Plase fill all required fields.")
-        return false;
-    }
-    else{
-        alert("Thank you for your submission. We'll get back to you soon.")
-        return true;
-    }
-
-}
-
-
-
 
 
 
     return(
         <div className= 'form-container'>
 
-            <form onSubmit= {myFunction}>
+            <form onSubmit= {sendEmail}>
                 <input type='text' placeholder="Name*" className= "here" id= "input1" name= "name"  />
                 <div class= "required-text"></div>
 
